c22240cf547aff3b41d5ac096e27ed8950416134
Yusoof <mohmedyusoof.as@cognizant.com>
2012-05-03 17:05:34 +0100

    import jar using gradle

---
+++ src/main/groovy/service/BaseService.groovy
@@ -0,0 +1,15 @@
+package service
+
+import groovyx.net.http.RESTClient
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: admin
+ * Date: 03/05/12
+ * Time: 16:46
+ * To change this template use File | Settings | File Templates.
+ */
+class BaseService {
+
+    RESTClient restClient
+}
